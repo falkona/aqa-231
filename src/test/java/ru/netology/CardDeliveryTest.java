ec827ab55be46c6a477c28162ec8c7757d0b9145
Dasha Nos <dasha.nos91@gmail.com>
2019-11-03 14:25:25 +0300

    shouldBeErrorIfPhoneIsInvalid added

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -21,8 +21,7 @@ public class CardDeliveryTest {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $$(".button").findBy(Condition.exactText("Запланировать")).click();
-        $("[data-test-id=success-notification]").waitUntil(Condition.visible, 15000);
-        Utils.sleep(5);
+        $("[data-test-id=success-notification]").waitUntil(Condition.visible, 3000);
     }
 
     @Test
@@ -36,11 +35,47 @@ public class CardDeliveryTest {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $$(".button").findBy(Condition.exactText("Запланировать")).click();
-        $("[data-test-id=success-notification]").waitUntil(Condition.visible, 15000);
-
+        $("[data-test-id=success-notification]").waitUntil(Condition.visible, 3000);
         $("button .icon_name_calendar").click();
         $(".calendar__layout").waitUntil(Condition.visible, 2000);
         long timestamp = DataGenerator.getTimeStampString(DataGenerator.getEarliestValidDate().plusDays(1));
         $(String.format("[data-day='%d000']", timestamp)).click();
+        $$(".button").findBy(Condition.exactText("Запланировать")).click();
+        $("[data-test-id=replan-notification]").waitUntil(Condition.visible, 3000);
+    }
+
+    @Test
+    void doNotNeedReplanIfDateWasNotChanged() {
+        open(baseUrl);
+
+        UserInfo validUser = DataGenerator.createValidUserInfo();
+
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=name] input").setValue(validUser.getFullName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $$(".button").findBy(Condition.exactText("Запланировать")).click();
+        $("[data-test-id=success-notification]").waitUntil(Condition.visible, 3000);
+        $("button .icon_name_calendar").click();
+        $(".calendar__layout").waitUntil(Condition.visible, 2000);
+        long timestamp = DataGenerator.getTimeStampString(DataGenerator.getEarliestValidDate());
+        $(String.format("[data-day='%d000']", timestamp)).click();
+        $$(".button").findBy(Condition.exactText("Запланировать")).click();
+        $("[data-test-id=replan-notification]").waitUntil(Condition.not(Condition.visible), 3000);
+    }
+
+    @Test
+    void shouldBeErrorIfPhoneIsInvalid() {
+        open(baseUrl);
+
+        UserInfo validUser = DataGenerator.createUserInvalidPhone();
+
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=name] input").setValue(validUser.getFullName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $$(".button").findBy(Condition.exactText("Запланировать")).click();
+        $("[data-test-id=phone]").shouldHave(Condition.cssClass("input_invalid"));
+        $("[data-test-id=success-notification]").waitUntil(Condition.not(Condition.visible), 3000);
     }
 }
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -4,7 +4,10 @@ import com.github.javafaker.Faker;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
     private DataGenerator() {}
@@ -12,14 +15,46 @@ public class DataGenerator {
     public static UserInfo createValidUserInfo () {
         Faker faker = new Faker(new Locale("ru"));
         return new UserInfo(
-                faker.address().city(),
+                getAdministrativeCenter(),
                 faker.name().fullName(),
                 faker.phoneNumber().phoneNumber()
         );
     }
 
+    public static UserInfo createUserInvalidPhone () {
+        Faker faker = new Faker(new Locale("ru"));
+        return new UserInfo(
+                getAdministrativeCenter(),
+                faker.name().fullName(),
+                getInvalidPhoneNumber()
+        );
+    }
+
     public static String getAdministrativeCenter() {
-        return "";
+        List<String> administrativeCenters = new ArrayList<>();
+        administrativeCenters.add("Москва");
+        administrativeCenters.add("Волгоград");
+        administrativeCenters.add("Санкт-Петербург");
+        administrativeCenters.add("Ростов-на-Дону");
+        administrativeCenters.add("Краснодар");
+        administrativeCenters.add("Владивосток");
+        administrativeCenters.add("Новосибирск");
+
+        Random random = new Random();
+        int index = random.nextInt(7);
+
+        return administrativeCenters.get(index);
+    }
+
+    public static String getInvalidPhoneNumber() {
+        List<String> invalidPhones = new ArrayList<>();
+        invalidPhones.add("+19025668899");
+        invalidPhones.add("+790256688");
+
+        Random random = new Random();
+        int index = random.nextInt(2);
+
+        return invalidPhones.get(index);
     }
 
     public static int getMinimumDaysToDelivery() {
